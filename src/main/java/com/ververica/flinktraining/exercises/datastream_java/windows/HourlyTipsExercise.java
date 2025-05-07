b513eb267b65fb95273b7292a1707859a0b66bf5
EdgarAbramian <91754043+EdgarAbramian@users.noreply.github.com>
2025-05-07 11:07:14 +0300

    Update HourlyTipsExercise.java

--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/windows/HourlyTipsExercise.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/windows/HourlyTipsExercise.java
@@ -1,41 +1,19 @@
-/*
- * Copyright 2018 data Artisans GmbH, 2019 Ververica GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ververica.flinktraining.exercises.datastream_java.windows;
 
 import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiFare;
 import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiFareSource;
 import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
-import com.ververica.flinktraining.exercises.datastream_java.utils.MissingSolutionException;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
-/**
- * The "Hourly Tips" exercise of the Flink training
- * (http://training.ververica.com).
- *
- * The task of the exercise is to first calculate the total tips collected by each driver, hour by hour, and
- * then from that stream, find the highest tip total in each hour.
- *
- * Parameters:
- * -input path-to-input-file
- *
- */
+
 public class HourlyTipsExercise extends ExerciseBase {
 
 	public static void main(String[] args) throws Exception {
@@ -55,12 +33,27 @@ public class HourlyTipsExercise extends ExerciseBase {
 		// start the data generator
 		DataStream<TaxiFare> fares = env.addSource(fareSourceOrTest(new TaxiFareSource(input, maxEventDelay, servingSpeedFactor)));
 
-		throw new MissingSolutionException();
+		DataStream<Tuple3<Long, Long, Float>> hourlyMax = fares
+				.keyBy((TaxiFare fare) -> fare.driverId)
+				.timeWindow(Time.hours(1))
+				.process(new SumTips())
+				.timeWindowAll(Time.hours(1))
+				.maxBy(2);
 
-//		printOrTest(hourlyMax);
+		printOrTest(hourlyMax);
+		env.execute("Hourly Tips (java)");
+	}
 
-		// execute the transformation pipeline
-//		env.execute("Hourly Tips (java)");
+	public static class SumTips extends ProcessWindowFunction<TaxiFare, Tuple3<Long, Long, Float>, Long, TimeWindow> {
+		@Override
+		public void process(Long key, Context context, Iterable<TaxiFare> fares, Collector<Tuple3<Long, Long, Float>> out)
+				throws Exception {
+			Float sum = 0F;
+			for (TaxiFare f : fares) {
+				sum += f.tip;
+			}
+			out.collect(new Tuple3<>(context.window().getEnd(), key, sum));
+		}
 	}
 
 }
